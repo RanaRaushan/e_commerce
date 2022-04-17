240415357d15aa4ad3b32eff59648ab10876ebf5
RanaRaushan <RanaRaushan.Kumar_EX@kdrp.com>
2022-04-17 13:22:35 +0530

    reverted url strategy

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # E-commerce System
 
-I have deployed on static web on "https://625bc1908c9f152defc0550c--creative-yeot-39509d.netlify.app"
+I have deployed static web on netlify at "https://625bc1908c9f152defc0550c--creative-yeot-39509d.netlify.app"
 
 Note: Poll System is available at "https://625bc1908c9f152defc0550c--creative-yeot-39509d.netlify.app/#/myPolls"
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,12 @@
 import 'package:e_commerce/models/wishlist_cart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 import 'app.dart';
 import 'models/item.dart';
 import 'models/shopping_cart.dart';
 
 void main() {
-  setPathUrlStrategy();
   runApp(
       MultiProvider(
         providers: [
